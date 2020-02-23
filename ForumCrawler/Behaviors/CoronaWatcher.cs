f9fc21238b95680f71e6230f9c7bafd0fbcd5520
Simon Farshid <Yonom@users.noreply.github.com>
2020-02-23 03:32:39 +0100

    fix death rate integer calculation

--- ForumCrawler/Behaviors/CoronaWatcher.cs
+++ ForumCrawler/Behaviors/CoronaWatcher.cs
@@ -42,7 +42,7 @@ namespace ForumCrawler
 
                         await client.GetGuild(DiscordSettings.GuildId).GetTextChannel(329335303963803649).ModifyAsync(c =>
                         {
-                            c.Topic = $"Casual Talk. COVID-19 Infected: {sumCases - sumDeaths - sumRecovered}, Recovered: {sumRecovered}, Deaths: {sumDeaths}, Death Rate: {sumDeaths / (sumDeaths + sumRecovered):P1}";
+                            c.Topic = $"Casual Talk. COVID-19 Infected: {sumCases - sumDeaths - sumRecovered}, Recovered: {sumRecovered}, Deaths: {sumDeaths}, Death Rate: {sumDeaths / (double)(sumDeaths + sumRecovered):P1}";
                         });
                     }
                 }
