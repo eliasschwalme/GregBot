073bfd6b512b273487aa41281e74d34b9ba79103
Simon Farshid <Yonom@users.noreply.github.com>
2020-05-08 02:08:08 +0200

    Fix roman conversion

--- ForumCrawler/Models/ScoreUser.cs
+++ ForumCrawler/Models/ScoreUser.cs
@@ -258,19 +258,25 @@ namespace ForumCrawler
         {
             if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value between 0 and 3999");
             if (number == 0) return "0";
-            if (number >= 1000) return "M" + ToRoman(number - 1000);
-            if (number >= 900) return "CM" + ToRoman(number - 900);
-            if (number >= 500) return "D" + ToRoman(number - 500);
-            if (number >= 400) return "CD" + ToRoman(number - 400);
-            if (number >= 100) return "C" + ToRoman(number - 100);
-            if (number >= 90) return "XC" + ToRoman(number - 90);
-            if (number >= 50) return "L" + ToRoman(number - 50);
-            if (number >= 40) return "XL" + ToRoman(number - 40);
-            if (number >= 10) return "X" + ToRoman(number - 10);
-            if (number >= 9) return "IX" + ToRoman(number - 9);
-            if (number >= 5) return "V" + ToRoman(number - 5);
-            if (number >= 4) return "IV" + ToRoman(number - 4);
-            if (number >= 1) return "I" + ToRoman(number - 1);
+            return ToRomanInner(number);
+        }
+
+        private static string ToRomanInner(int number)
+        {
+            if (number == 0) return "";
+            if (number >= 1000) return "M" + ToRomanInner(number - 1000);
+            if (number >= 900) return "CM" + ToRomanInner(number - 900);
+            if (number >= 500) return "D" + ToRomanInner(number - 500);
+            if (number >= 400) return "CD" + ToRomanInner(number - 400);
+            if (number >= 100) return "C" + ToRomanInner(number - 100);
+            if (number >= 90) return "XC" + ToRomanInner(number - 90);
+            if (number >= 50) return "L" + ToRomanInner(number - 50);
+            if (number >= 40) return "XL" + ToRomanInner(number - 40);
+            if (number >= 10) return "X" + ToRomanInner(number - 10);
+            if (number >= 9) return "IX" + ToRomanInner(number - 9);
+            if (number >= 5) return "V" + ToRomanInner(number - 5);
+            if (number >= 4) return "IV" + ToRomanInner(number - 4);
+            if (number >= 1) return "I" + ToRomanInner(number - 1);
             throw new ArgumentOutOfRangeException("Could not convert to roman");
         }
     }
