ef21cf4325648ee072b86e230a051a7167b7f88d
Simon Farshid <Yonom@users.noreply.github.com>
2020-05-09 14:07:02 +0200

    Add migration

---
+++ ForumCrawler/Migrations/202005091205228_RemoveExemptAndShowScore.cs
@@ -0,0 +1,20 @@
+﻿namespace ForumCrawler.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveExemptAndShowScore : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.ScoreUsers", "EarlyUserExempt");
+            DropColumn("dbo.ScoreUsers", "ShowInUsername");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ScoreUsers", "ShowInUsername", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ScoreUsers", "EarlyUserExempt", c => c.Boolean(nullable: false));
+        }
+    }
+}
