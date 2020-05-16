5a616cf4e3ea788daad95e25e232f2c94032fa7c
Simon Farshid <Yonom@users.noreply.github.com>
2020-05-16 02:09:17 +0200

    Update database

---
+++ ForumCrawler/Migrations/202005160009042_AddStreaks.cs
@@ -0,0 +1,22 @@
+﻿namespace ForumCrawler.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStreaks : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ScoreUsers", "DailyCount", c => c.Int(nullable: false));
+            AddColumn("dbo.ScoreUsers", "DailyStreakCount", c => c.Int(nullable: false));
+            AddColumn("dbo.ScoreUsers", "HasDisabledThresholdWarning", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ScoreUsers", "HasDisabledThresholdWarning");
+            DropColumn("dbo.ScoreUsers", "DailyStreakCount");
+            DropColumn("dbo.ScoreUsers", "DailyCount");
+        }
+    }
+}
