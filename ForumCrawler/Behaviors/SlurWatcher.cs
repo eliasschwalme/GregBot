a2361699fde6a6cc75510513b6d47cc713a66278
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-06-04 19:52:01 -0400

    get slurtest back to working

--- ForumCrawler/Behaviors/SlurWatcher.cs
+++ ForumCrawler/Behaviors/SlurWatcher.cs
@@ -1,4 +1,6 @@
 ﻿using Discord.WebSocket;
+
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ForumCrawler.Behaviors
@@ -6,7 +8,9 @@ namespace ForumCrawler.Behaviors
 	public static class SlurWatcher
 	{
         // "#________SLURTEST________#" is for moderators to test if the slur filter is working
-        public static string[] Slurs = new string[] { "nigger", "nigga", "niqqer", "niqqa", "faggot", "#________SLURTEST________#" };
+        public static string[] Slurs = new string[] { "nigger", "nigga", "niqqer", "niqqa", "faggot", "#SLURTEST#" }
+            .Select(slur => slur.ToLower())
+            .ToArray();
 
         public static void Bind(DiscordSocketClient client)
         {
