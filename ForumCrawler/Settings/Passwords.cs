a2ae94e891fdab9208f40cc3115c4c8dfd40f737
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-08-16 16:20:48 -0400

    Configuration App Stuff

--- ForumCrawler/Settings/Passwords.cs
+++ ForumCrawler/Settings/Passwords.cs
@@ -1,12 +1,16 @@
-﻿namespace ForumCrawler
+﻿using System.Configuration;
+
+namespace ForumCrawler
 {
     public static class Passwords
     {
-        #region hidden
+        static Passwords()
+        {
+            ForumPassword = ConfigurationManager.AppSettings["ForumPassword"];
+            DiscordToken = ConfigurationManager.AppSettings["DiscordToken"];
+        }
 
-        public const string ForumPassword = "y36zDt440ijG";
-        public const string DiscordToken = "MzI4NTcxODA2MDExMjI4MTYy.XY0vyA.DASbJX7n6YntZQiQGtV1kVfftag";
-
-        #endregion hidden
+        public static string ForumPassword;
+        public static string DiscordToken;
     }
 }
